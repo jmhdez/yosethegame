43ec6fa47155eab70ff3a1764e70df430e13c9e1
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-02-26 16:06:18 +0100

    solved second exercise

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler yosethegame.handler/app}
   :profiles
--- src/yosethegame/handler.clj
+++ src/yosethegame/handler.clj
@@ -1,14 +1,18 @@
 (ns yosethegame.handler
   (:use compojure.core)
   (:use hiccup.core)
+  (:use ring.middleware.json)
+  (:use ring.util.response)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
 (defroutes app-routes
   (GET "/" [] "Hello Yose")
-  (GET "/ping" [] "{ \"alive\": true }")
+  (GET "/ping" [] (response {:alive true}))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (handler/site (-> app-routes
+                    wrap-json-response
+                    wrap-json-body)))
