4e2a32f8fa74007f2d46a8a2c910632d69dc1366
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-02-27 15:41:34 +0100

    added contact link

--- src/yosethegame/handler.clj
+++ src/yosethegame/handler.clj
@@ -2,11 +2,19 @@
   (:use compojure.core)
   (:use ring.middleware.json)
   (:use ring.util.response)
+  (:use hiccup.page)
+  (:use hiccup.element)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn home []
+  (html5
+   [:body
+    [:h1 "Hello Yose"]
+    [:a#contact-me-link {:href "#"}  "Contact me" ]]))
+
 (defroutes app-routes
-  (GET "/" [] "Hello Yose")
+  (GET "/" [] (home))
   (GET "/ping" [] (response {:alive true})))
 
 (def app
