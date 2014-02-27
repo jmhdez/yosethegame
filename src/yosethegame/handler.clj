10a83a519aac2303ad3c44bcc5a407c5d01abcf3
jmhdez <juan.hernandez.arroyo@gmail.com>
2014-02-27 16:06:26 +0100

    added factorization for power of two

--- src/yosethegame/handler.clj
+++ src/yosethegame/handler.clj
@@ -14,11 +14,22 @@
     [:a#contact-me-link {:href "#"}  "Contact me" ]
     [:a#ping-challenge-link {:href "/ping"} "Ping challenge"]]))
 
+(defn ping []
+  (response {:alive true}))
+
+(defn prime-factors [n]
+  (let [factors (map (fn [_] 2)
+                     (take-while #(not= % n) (iterate #(* 2 %) 1)))]
+     (response {:number n
+                :decomposition factors})))
+
 (defroutes app-routes
   (GET "/" [] (home))
-  (GET "/ping" [] (response {:alive true})))
+  (GET "/ping" [] (ping))
+  (GET "/primeFactors" {{n "number"} :query-params} (prime-factors (read-string n))))
 
 (def app
   (handler/site (-> app-routes
                     wrap-json-response
                     wrap-json-body)))
+
